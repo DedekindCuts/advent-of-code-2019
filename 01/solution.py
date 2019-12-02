5e0a8431baeff08e5df35c22b1011e541732f8a9
Nick Larsen <nick.larsen100@gmail.com>
2019-12-01 20:32:50 -0700

    Improve formatting

--- 01/solution.py
+++ 01/solution.py
@@ -1,7 +1,9 @@
 import math
 
 def fuelCost(mass):
-  """function to calculate fuel cost for a given mass"""
+  """
+  function to calculate fuel cost for a given mass
+  """
 
   return (math.floor(mass / 3) - 2)
 
@@ -25,16 +27,13 @@ def trueFuelCost(mass):
 with open("01/input.txt") as input_file:
   lines = [line.rstrip('\n') for line in input_file]
 
+# calculate total fuel cost
 masses = [int(line) for line in lines]
-
 costs = [fuelCost(mass) for mass in masses]
-
 total_cost = sum(costs)
-
 print("Total cost: " + str(total_cost))
 
+# calculate total fuel cost taking weight of fuel into account
 true_costs = [trueFuelCost(mass) for mass in masses]
-
 true_total_cost = sum(true_costs)
-
 print("True total cost: " + str(true_total_cost))
