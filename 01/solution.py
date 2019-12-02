4dc831ad407b099140113d20c75b6c6b7b73364c
Nick Larsen <n.larsen@veracityfa.com>
2019-12-01 18:39:12 -0700

    Initial commit; day 1 solutions

---
+++ 01/solution.py
@@ -0,0 +1,40 @@
+import math
+
+def fuelCost(mass):
+  """function to calculate fuel cost for a given mass"""
+
+  return (math.floor(mass / 3) - 2)
+
+def trueFuelCost(mass):
+  """
+  function to calculate fuel cost for a given mass after taking mass of fuel 
+  into account
+  """
+
+  total = 0
+  cost = mass
+
+  while True:
+    cost = max(fuelCost(cost), 0)
+    total += cost
+    if cost == 0:
+      break
+
+  return total
+
+with open("01/input.txt") as input_file:
+  lines = [line.rstrip('\n') for line in input_file]
+
+masses = [int(line) for line in lines]
+
+costs = [fuelCost(mass) for mass in masses]
+
+total_cost = sum(costs)
+
+print("Total cost: " + str(total_cost))
+
+true_costs = [trueFuelCost(mass) for mass in masses]
+
+true_total_cost = sum(true_costs)
+
+print("True total cost: " + str(true_total_cost))
